8a67c7c540fc49e4968023b28a79fd588ee8533a
nik123-py <nik488151@gmail.com>
2026-02-16 11:08:08 +0530

    UC1 :application entry and welcome message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+/**
+ * =====================================================
+ * MAIN CLASS - UseCase1PalindromeApp
+ * =====================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * No palindrome logic is implemented yet.
+ * @author nik123-py
+ *
+ *
+ *
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
